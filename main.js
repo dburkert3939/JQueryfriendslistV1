55a76de1690033d4e5d39ff8b1ae9afc698e3778
Dustin Burkert <dustinburkert5@gmail.com>
2018-01-16 18:46:07 -0500

    module 03 - Remove friends

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
         $('#friend-list').append(newElement);
         $('#input').val("");
     })
+    $('#friend-list').on('click', 'li', function(e) {
+        console.log('clicked!');
+        $(this).remove();
+    })
 
     function createFriend(name) {
         return $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark');
